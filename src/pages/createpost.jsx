7f5a10f1d3383f9530e76609adabeb68fcefdf3d
sk sk <kadyrbekovasaadat0@gmail.com>
2023-10-24 10:15:24 +0600

    дополнили первое дз, прописали функционал для компоненты creatpost, если запрос правильный то useNavigate перебрасывает нас на главную страницу с постами

--- src/pages/createpost.jsx
+++ src/pages/createpost.jsx
@@ -1,13 +1,28 @@
 import React, {useState} from 'react';
+import axios from "axios";
+import {useNavigate} from "react-router-dom";
 
 const Createpost = () => {
     const [inputData, setInputData ] = useState('')
+    const navigate = useNavigate()
+    const [postResponse, setPostResponse] = useState({})
 
+    const postCreateFunc = () => {
+        axios.post('https://dummyjson.com/posts/add',{
+            title: inputData,
+            userId: '1'
+        })
+            .then((response) => {
+                setPostResponse(response.data)
+                navigate('/')
+            }).catch(err => {console.error(err)})
+
+    }
 
     return (
         <div>
             <input type="text" value={inputData} onChange={e => setInputData(e.target.value)}/>
-            <button>Создать пост</button>
+            <button onClick={postCreateFunc}>Создать пост</button>
         </div>
     );
 };
